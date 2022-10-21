c22ee611c200e03c8f795e2d8e7538cbe4fc43cb
dav21016 <dav21016@byui.edu>
2022-10-20 23:13:46 -0600

    just a few more fixes and we should be good to go

--- Program.cs
+++ Program.cs
@@ -1,21 +1,2 @@
-﻿using unit03_jumper;
-
-
-namespace unit03_jumper
-{
-    /// <summary>
-    /// The program's entry point.
-    /// </summary>
-    class Program
-    {
-        /// <summary>
-        /// Starts the program using the given arguments.
-        /// </summary>
-        /// <param name="args">The given arguments.</param>
-        static void Main(string[] args)
-        {
-            Director director = new Director();
-            director.StartGame();
-        }
-    }
-}
+﻿var jumper = new Player();
+jumper.StartGame();
---
+++ wordpicker.cs
@@ -0,0 +1,21 @@
+// Class to create a list with words and return a random word from it
+public class WordPicker {
+    List<string> wordsList = new List<string>();
+    string randomWord;
+    int index;
+    Random random = new Random();
+    
+    public WordPicker() {
+        wordsList.Add("dog");
+        wordsList.Add("cat");
+        wordsList.Add("bird");
+        wordsList.Add("duck");
+
+        index = random.Next(wordsList.Count);
+    }
+
+    
+    public string pickRandomWord() {
+        return wordsList[index];
+    }
+}
