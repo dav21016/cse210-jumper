fdde531f81ebaef577c144ea688a392e031f8102
dav21016 <dav21016@byui.edu>
2022-10-21 22:50:56 -0600

    private class

--- Player.cs
+++ Player.cs
@@ -63,7 +63,7 @@ public class Player {
         jumper.DrawJumper(list);
     }
 
-    public string getLetter(string? randomWord) {
+    private string getLetter(string? randomWord) {
         
         Console.WriteLine();
         Console.WriteLine();
